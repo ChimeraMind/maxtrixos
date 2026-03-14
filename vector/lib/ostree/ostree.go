d7effa2e3413a8cf0d6c5696682418a372b15617
Fabio Erculiani <m@lxnay.me>
2026-03-14 11:25:22 +0100

    Add initial implementation of the Ostree package with core functionality and interface definitions

--- vector/lib/ostree/ostree.go
+++ vector/lib/ostree/ostree.go
@@ -1,8 +1,8 @@
 package ostree
 
 import (
-	"bufio"
 	"bytes"
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -16,165 +16,76 @@ import (
 // IOstree defines the interface for ostree operations.
 // It mirrors all public methods of Ostree for testability.
 type IOstree interface {
-
-	// CloneForRef creates a new IOstree instance with the same configuration but a different ref.
-	CloneForRef(ref string) (IOstree, error)
-
-	// SetStdout sets the stdout writer for the Ostree instance.
+	// Styled output
 	SetStdout(io.Writer)
-	// SetStderr sets the stderr writer for the Ostree instance.
 	SetStderr(io.Writer)
-	// Print prints to stdout with the given format and arguments.
-	Print(format string, a ...any)
-	// PrintError prints to stderr with the given format and arguments.
-	PrintError(format string, a ...any)
+	Print(format string, a ...interface{})
+	PrintError(format string, a ...interface{})
 
-	// Ref returns the ref for the Ostree instance.
-	Ref() string
-	// SetRef sets the ref for the Ostree instance.
-	SetRef(ref string)
-
-	// FullBranchSuffix returns the configured suffix used to identify "full" branches.
+	// Config accessors
 	FullBranchSuffix() (string, error)
-	// IsBranchFullSuffixed checks if the instance ref is a "full" branch.
-	IsBranchFullSuffixed() (bool, error)
-	// BranchShortnameToFull converts a short branch name to a full one.
+	IsBranchFullSuffixed(ref string) (bool, error)
 	BranchShortnameToFull(shortName, relStage, osName, arch string) (string, error)
-	// BranchToFull converts a normal branch name to a full one.
-	BranchToFull() (string, error)
-	// RemoveFullFromBranch removes the "-full" suffix from a branch name.
-	RemoveFullFromBranch() (string, error)
-	// GpgEnabled returns whether GPG signing and verification is enabled.
+	BranchToFull(ref string) (string, error)
+	RemoveFullFromBranch(ref string) (string, error)
 	GpgEnabled() (bool, error)
-	// GpgPrivateKeyPath returns the user defined private/ placed GPG private key path.
 	GpgPrivateKeyPath() (string, error)
-	// GpgPublicKeyPath returns the user defined private/ placed GPG public key path.
 	GpgPublicKeyPath() (string, error)
-	// GpgOfficialPubKeyPath returns the official, git repository distributed GPG public key path.
 	GpgOfficialPubKeyPath() (string, error)
-	// OsName returns the name of the OS as defined in the config.
 	OsName() (string, error)
-	// FancyOsName returns the fancy (display) name of the OS as defined in the config.
-	FancyOsName() (string, error)
-	// Arch returns the build architecture as defined in the config.
 	Arch() (string, error)
-	// RepoDir returns the path to the ostree repository.
 	RepoDir() (string, error)
-	// Sysroot returns the path to the ostree sysroot directory. Usually /sysroot.
 	Sysroot() (string, error)
-	// Root returns the path to the root filesystem directory used as root for
-	// ostree operations (i.e. --sysroot).
 	Root() (string, error)
-	// Remote returns the name of the remote.
 	Remote() (string, error)
-	// RemoteURL returns the URL of the remote.
 	RemoteURL() (string, error)
-	// AvailableGpgPubKeyPaths returns the list of available (file exists)
-	// GPG public key paths.
 	AvailableGpgPubKeyPaths() ([]string, error)
-	// GpgBestPubKeyPath returns the path to the GPG public key to use.
-	// It prefers the private key path over the official one.
 	GpgBestPubKeyPath() (string, error)
-	// ClientSideGpgArgs returns arguments for client-side GPG verification.
 	ClientSideGpgArgs() ([]string, error)
-	// GpgHomeDir returns the path to the GPG homedir, creating and setting permissions if needed.
 	GpgHomeDir() (string, error)
-	// GpgKeyID returns the GPG key ID to use for signing.
 	GpgKeyID() (string, error)
-	// GpgArgs returns the gpg arguments for ostree commands.
 	GpgArgs() ([]string, error)
-	// SetGpg enables or disables GPG verification in the local ostree repository.
-	SetGpg(enabled bool) error
 
-	// SetupEtc moves the /etc directory to /usr/etc.
-	SetupEtc(rootfs string) error
-	// PrepareFilesystemHierarchy prepares the filesystem hierarchy for OSTree.
-	PrepareFilesystemHierarchy(rootfs string) error
-	// ValidateFilesystemHierarchy validates the filesystem hierarchy for OSTree.
-	ValidateFilesystemHierarchy(rootfs string) error
+	// Filesystem operations
+	SetupEtc(imageDir string) error
+	PrepareFilesystemHierarchy(imageDir string) error
+	ValidateFilesystemHierarchy(imageDir string) error
 
-	// SetVerbose sets the verbose flag for the Ostree instance.
-	SetVerbose(bool)
-
-	// Commit performs an ostree commit using the instance runner.
-	Commit(opts CommitOptions) error
-	// InitRepo initialises the local ostree repository in archive mode.
-	InitRepo() error
-	// BootCommit returns the boot commit from an ostree sysroot.
+	// Repo operations
 	BootCommit(sysroot string) (string, error)
-	// ListRemotes lists all the remote refs in the configuration's ostree repository.
-	ListRemotes() ([]string, error)
-	// LastCommit returns the last commit for the instance ref.
-	LastCommit() (string, error)
-	// ImportGpgKey imports a GPG key into the GPG homedir.
+	ListRemotes(verbose bool) ([]string, error)
+	LastCommit(ref string, verbose bool) (string, error)
 	ImportGpgKey(keyPath string) error
-	// GpgSignFile signs a file with GPG.
 	GpgSignFile(file string) error
-	// GpgKeys returns the list of GPG key paths used for signing and verification.
 	GpgKeys() ([]string, error)
-	// InitializeSigningGpg imports GPG keys into the local GPG keyring.
-	InitializeSigningGpg() error
-	// MaybeInitializeGpg initializes GPG keys for an ostree repository.
-	MaybeInitializeGpg() error
-	// MaybeInitializeRemote initializes an ostree remote.
-	MaybeInitializeRemote() error
-	// Pull pulls the instance ref from its configured remote.
-	Pull() error
-	// Prune prunes the ostree repo for the instance ref.
-	Prune() error
-	// GenerateStaticDelta generates a static delta for an ostree repository.
-	GenerateStaticDelta() error
-	// UpdateSummary updates the summary of an ostree repository.
-	UpdateSummary() error
-	// AddRemote adds a remote to an ostree repo.
-	AddRemote() error
-	// AddRemoteToRootfs adds a remote to an ostree rootfs.
-	AddRemoteToRootfs(rootfs string) error
-	// LocalRefs lists the locally available ostree refs.
-	LocalRefs() ([]string, error)
-	// RemoteRefs lists the remote available ostree refs.
-	RemoteRefs() ([]string, error)
-	// ListDeployments lists the deployments in the / filesystem.
-	ListDeployments() ([]Deployment, error)
-	// DeployedRootfs returns the path to the deployed rootfs.
-	DeployedRootfs() (string, error)
-	// BootedRef returns the ref of the booted deployment.
-	BootedRef() (string, error)
-	// BootedHash returns the commit hash of the booted deployment.
-	BootedHash() (string, error)
-	// Switch runs `ostree admin switch` to switch to the instance ref.
-	Switch() error
-	// Deploy deploys an ostree commit.
-	Deploy(sysroot string, bootArgs []string) error
-	// Upgrade runs `ostree admin upgrade`.
-	Upgrade(args []string) error
-	// Pin pins a deployment by index so that it can be persisted as rollback target.
-	Pin(targetIndex int) error
-	// Unpin removes a previously pinned deployment.
-	Unpin(targetIndex int) error
-	// ListPackages lists the packages in a commit.
-	ListPackages(commit string) ([]string, error)
-	// ListContents lists the contents of a path in a commit.
-	ListContents(commit, path string) (*[]filesystems.PathInfo, error)
-	// ListEtcChanges performs a 3-way diff between the old pristine /usr/etc,
-	// the new pristine /usr/etc, and the user's live /etc, and returns a list of
-	// changes with their classification (add/update/remove/conflict/user-only).
-	ListEtcChanges(aCommit, bCommit string) ([]EtcChange, error)
+	InitializeSigningGpg(verbose bool) error
+	InitializeRemoteSigningGpg(remote, repoDir string, verbose bool) error
+	MaybeInitializeGpg(verbose bool) error
+	MaybeInitializeGpgForRepo(remote, repoDir string, verbose bool) error
+	MaybeInitializeRemote(verbose bool) error
+	Pull(ref string, verbose bool) error
+	PullWithRemote(remote, ref string, verbose bool) error
+	Prune(ref string, verbose bool) error
+	GenerateStaticDelta(ref string, verbose bool) error
+	UpdateSummary(verbose bool) error
+	AddRemote(verbose bool) error
+	AddRemoteToRootfs(rootfs string, verbose bool) error
+	LocalRefs(verbose bool) ([]string, error)
+	RemoteRefs(verbose bool) ([]string, error)
+	ListDeployments(verbose bool) ([]Deployment, error)
+	DeployedRootfs(ref string, verbose bool) (string, error)
+	BootedRef(verbose bool) (string, error)
+	BootedHash(verbose bool) (string, error)
+	Switch(ref string, verbose bool) error
+	Deploy(ref, sysroot string, bootArgs []string, verbose bool) error
+	Upgrade(args []string, verbose bool) error
+	ListPackages(commit string, verbose bool) ([]string, error)
+	ListContents(commit, path string, verbose bool) (*[]filesystems.PathInfo, error)
+	ListEtcChanges(oldSHA, newSHA string) ([]EtcChange, error)
 }
 
 // runCommand runs a generic binary with args and stdout/stderr handling.
 var runCommand runner.Func = runner.Run
-
-// newOstreeCmd creates a runner.Cmd for the "ostree" binary with the given
-// stdout/stderr writers and arguments.
-func newOstreeCmd(stdout, stderr io.Writer, args ...string) *runner.Cmd {
-	return &runner.Cmd{
-		Name:   "ostree",
-		Args:   args,
-		Stdout: stdout,
-		Stderr: stderr,
-	}
-}
 
 func readerToList(reader io.Reader) ([]string, error) {
 	var elements []string
@@ -219,21 +130,17 @@ func SetupEnvironment() {
 }
 
 type Ostree struct {
-	cfg     config.IConfig
-	stdout  io.Writer
-	stderr  io.Writer
-	runner  runner.Func
-	verbose bool
-	ref     string
+	cfg    config.IConfig
+	stdout io.Writer
+	stderr io.Writer
+	runner runner.Func
 }
 
 // NewOstreeWithRunner creates a new Ostree instance with a custom command runner (for testing).
 type NewOstreeOptions struct {
-	Config  config.IConfig
-	Stdout  io.Writer
-	Stderr  io.Writer
-	Verbose bool
-	Ref     string
+	Config config.IConfig
+	Stdout io.Writer
+	Stderr io.Writer
 }
 
 // NewOstree creates a new Ostree instance.
@@ -250,80 +157,57 @@ func NewOstree(opts NewOstreeOptions) (*Ostree, error) {
 		stderr = os.Stderr
 	}
 	return &Ostree{
-		cfg:     opts.Config,
-		stdout:  stdout,
-		stderr:  stderr,
-		runner:  runCommand,
-		verbose: opts.Verbose,
-		ref:     opts.Ref,
+		cfg:    opts.Config,
+		stdout: stdout,
+		stderr: stderr,
+		runner: runCommand,
 	}, nil
 }
 
-func (o *Ostree) CloneForRef(ref string) (IOstree, error) {
-	return NewOstree(NewOstreeOptions{
-		Config:  o.cfg,
-		Stdout:  o.stdout,
-		Stderr:  o.stderr,
-		Verbose: o.verbose,
-		Ref:     ref,
-	})
-}
-
+// SetStdout sets the stdout writer for the Ostree instance.
 func (o *Ostree) SetStdout(w io.Writer) {
 	o.stdout = w
 }
 
+// SetStderr sets the stderr writer for the Ostree instance.
 func (o *Ostree) SetStderr(w io.Writer) {
 	o.stderr = w
 }
 
-func (o *Ostree) Print(format string, a ...any) {
+// Print prints to stdout with the given format and arguments.
+func (o *Ostree) Print(format string, a ...interface{}) {
 	fmt.Fprintf(o.stdout, format, a...)
 }
 
-func (o *Ostree) PrintError(format string, a ...any) {
+// PrintError prints to stderr with the given format and arguments.
+func (o *Ostree) PrintError(format string, a ...interface{}) {
 	fmt.Fprintf(o.stderr, format, a...)
-}
-
-func (o *Ostree) SetVerbose(v bool) {
-	o.verbose = v
-}
-
-func (o *Ostree) Ref() string {
-	return o.ref
-}
-
-func (o *Ostree) SetRef(ref string) {
-	o.ref = ref
 }
 
 // runCmd runs a command via the instance's command runner, adding --verbose
 // and the "ostree" binary name automatically.
-func (o *Ostree) runCmd(stdout, stderr io.Writer, args ...string) error {
+func (o *Ostree) runCmd(stdout, stderr io.Writer, verbose bool, args ...string) error {
 	var finalArgs []string
-	if o.verbose {
+	if verbose {
 		finalArgs = append(finalArgs, "--verbose")
 		o.PrintError(">> Executing: ostree --verbose %s\n", strings.Join(args, " "))
 	}
 	finalArgs = append(finalArgs, args...)
-	return o.runner(newOstreeCmd(stdout, stderr, finalArgs...))
+	return o.runner(nil, stdout, stderr, "ostree", finalArgs...)
 }
 
 // ostreeRun runs an ostree command with stdout/stderr directed to the instance's stdout/stderr.
-func (o *Ostree) ostreeRun(args ...string) error {
-	return o.runCmd(o.stdout, o.stderr, args...)
+func (o *Ostree) ostreeRun(verbose bool, args ...string) error {
+	return o.runCmd(o.stdout, o.stderr, verbose, args...)
 }
 
 // ostreeRunCapture runs an ostree command and captures its stdout.
-func (o *Ostree) ostreeRunCapture(args ...string) (io.Reader, error) {
-	if o.verbose {
+func (o *Ostree) ostreeRunCapture(verbose bool, args ...string) (io.Reader, error) {
+	if verbose {
 		o.PrintError(">> Executing: ostree (stdout capture) %s\n", strings.Join(args, " "))
 	}
 	stdo := new(bytes.Buffer)
-	verbose := o.verbose
-	o.verbose = false
-	err := o.runCmd(stdo, o.stderr, args...)
-	o.verbose = verbose
+	err := o.runCmd(stdo, o.stderr, false, args...)
 	return stdo, err
 }
 
@@ -334,7 +218,7 @@ func run(stdout, stderr io.Writer, verbose bool, args ...string) error {
 		fmt.Fprintf(stderr, ">> Executing: ostree --verbose %s\n", strings.Join(args, " "))
 	}
 	finalArgs = append(finalArgs, args...)
-	return runCommand(newOstreeCmd(stdout, stderr, finalArgs...))
+	return runCommand(nil, stdout, stderr, "ostree", finalArgs...)
 }
 
 // Run runs an ostree command with --verbose if requested.
