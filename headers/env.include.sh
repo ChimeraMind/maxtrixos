a21af9c4f894114d4fdf3fd2cbb99d90aeeb0609
Fabio Erculiani <m@lxnay.me>
2026-03-15 11:58:38 +0100

    Remove MATRIXOS_GIT_REPO variable: eliminate unused Git repository variable from env.include.sh

--- headers/env.include.sh
+++ headers/env.include.sh
@@ -18,7 +18,6 @@ MATRIXOS_OVERLAY_GIT_REPO=$(env_lib.get_simple_var "matrixOS" "OverlayGitRepo")
 MATRIXOS_PRIVATE_EXAMPLE_GIT_REPO=$(env_lib.get_simple_var "matrixOS" "PrivateExampleGitRepo")
 MATRIXOS_PRIVATE_GIT_REPO_PATH=$(env_lib.get_root_var "${MATRIXOS_DEV_DIR}" "matrixOS" "PrivateGitRepoPath")
 MATRIXOS_DEFAULT_PRIVATE_GIT_REPO_PATH=$(env_lib.get_root_var "${MATRIXOS_DEV_DIR}" "matrixOS" "DefaultPrivateGitRepoPath")
-MATRIXOS_GIT_REPO=$(env_lib.get_simple_var "matrixOS" "GitRepo")
 
 ## Seeders section
 MATRIXOS_SEEDER_GPG_KEYS_DIR=$(env_lib.get_root_var "${MATRIXOS_ARTIFACTS_DIR}" "Seeder" "GpgKeysDir")
