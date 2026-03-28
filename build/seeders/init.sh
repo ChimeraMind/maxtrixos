2f09e6383a7870c187bb2e0e87d3fcf9c598e7f9
Fabio Erculiani <m@lxnay.me>
2026-03-28 10:45:04 +0100

    Refactor chroot setup functions and enhance tests for directory creation and mounting.
    Move all mounting logic to init.sh.

--- build/seeders/init.sh
+++ build/seeders/init.sh
@@ -30,9 +30,12 @@
 # $3..$n: The arguments to pass to the seeding script.
 set -eu
 
-
-setup_chroot_env() {
+maybe_mount_distfiles() {
     local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount distfiles." >&2
+        return 1
+    fi
 
     # Mount the necessary filesystems for the chroot environment.
     local dst_distfiles="${chroot_dir%/}/var/cache/distfiles"
@@ -49,6 +52,14 @@ setup_chroot_env() {
     else
         echo "${dst_distfiles} already mounted, skipping bind mount."
     fi
+}
+
+maybe_mount_binpkgs() {
+    local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount binpkgs." >&2
+        return 1
+    fi
 
     local dst_binpkgs="${chroot_dir%/}/var/cache/binpkgs"
     mkdir -p "${SEEDER_BINPKGS_DIR}"
@@ -64,6 +75,14 @@ setup_chroot_env() {
     else
         echo "${dst_binpkgs} already mounted, skipping bind mount."
     fi
+}
+
+maybe_mount_private_git() {
+    local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount private git repository." >&2
+        return 1
+    fi
 
     local dst_private_git="${chroot_dir%/}/${DEFAULT_PRIVATE_GIT_REPO_PATH#/}"
     mkdir -p "${dst_private_git}"
@@ -77,7 +96,92 @@ setup_chroot_env() {
     else
         echo "${dst_private_git} already mounted, skipping bind mount."
     fi
+}
 
+maybe_mount_sys() {
+    local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount sysfs." >&2
+        return 1
+    fi
+
+    local dst_sys="${chroot_dir%/}/sys"
+    mkdir -p "${dst_sys}"
+    if ! mountpoint -q "${dst_sys}"; then
+        echo "Mounting sysfs on ${dst_sys} ..."
+        mount -t sysfs sys "${dst_sys}"
+    else
+        echo "${dst_sys} is already mounted on the host, skipping mounting it inside chroot." >&2
+    fi
+}
+
+maybe_mount_dev() {
+    local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount dev." >&2
+        return 1
+    fi
+
+    local dst_dev="${chroot_dir%/}/dev"
+    if ! mountpoint -q "${dst_dev}"; then
+        echo "Mounting devtmpfs on ${dst_dev} ..."
+
+        mkdir -p "${dst_dev}"
+        mount -t devtmpfs devtmpfs "${dst_dev}"
+        # This is not necessary starting util-linux 2.27 since unshare
+        # enforces --propagation private by default. But let's keep this for
+        # a bit to be on the safe side.
+        mount --make-private "${dst_dev}"
+
+        echo "Mounting devpts on ${dst_dev}/pts ..."
+        mount -t devpts devpts "${dst_dev}/pts" -o rw,nosuid,noexec,relatime,gid=5,mode=600,ptmxmode=000
+        # This is not necessary starting util-linux 2.27 since unshare
+        # enforces --propagation private by default. But let's keep this for
+        # a bit to be on the safe side.
+        mount --make-private "${dst_dev}/pts"
+
+        echo "Mounting tmpfs on ${dst_dev}/shm ..."
+        mount -t tmpfs devshm "${dst_dev}/shm" -o rw,nosuid,nodev,mode=1777
+        # This is not necessary starting util-linux 2.27 since unshare
+        # enforces --propagation private by default. But let's keep this for
+        # a bit to be on the safe side.
+        mount --make-private "${dst_dev}/shm"
+    else
+        echo "${dst_dev} is already mounted on the host, skipping mounting it inside chroot." >&2
+    fi
+}
+
+maybe_mount_run_lock() {
+    local chroot_dir="${1}"
+    if [ -z "${chroot_dir}" ]; then
+        echo "chroot_dir is not set, unable to mount /run/lock." >&2
+        return 1
+    fi
+
+    local dst_run_lock="${chroot_dir%/}/run/lock"
+    if ! mountpoint -q "${dst_run_lock}"; then
+        echo "Mounting tmpfs on ${dst_run_lock} ..."
+
+        mkdir -p "${dst_run_lock}"
+        mount -t tmpfs run "${dst_run_lock}" -o rw,nosuid,nodev,noexec,relatime,size=5M
+        # This is not necessary starting util-linux 2.27 since unshare
+        # enforces --propagation private by default. But let's keep this for
+        # a bit to be on the safe side.
+        mount --make-private "${dst_run_lock}"
+    else
+        echo "${dst_run_lock} is already mounted on the host, skipping mounting it inside chroot." >&2
+    fi
+}
+
+setup_chroot_env() {
+    local chroot_dir="${1}"
+
+    maybe_mount_distfiles "${chroot_dir}"
+    maybe_mount_binpkgs "${chroot_dir}"
+    maybe_mount_private_git "${chroot_dir}"
+    maybe_mount_sys "${chroot_dir}"
+    maybe_mount_dev "${chroot_dir}"
+    maybe_mount_run_lock "${chroot_dir}"
     # The rest of the filesystems are mounted by the callee seeding script.
 }
 
